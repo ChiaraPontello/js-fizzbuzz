416cb05185cefea7cb2cfe704718ebd642c38976
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-14 16:54:12 +0200

    boxes e box

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 //riprendo classe boxes
-const boxesEl = document.querySelector('.boxes');
+const boxes = document.querySelector('.boxes');
 
 
 
@@ -11,7 +11,9 @@ for (let i = 1; i <= 100; i++) {
   const box = document.createElement('div');
   box.classList.add('box');
   
-}
+//collegamento tra boxes e box
+  boxes.append(box);
   
   
   
+};
