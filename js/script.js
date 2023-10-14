6746d0db0e00a29e833d63f221b10d48cf812bda
ChiaraPontello <chiarapontello1@gmail.com>
2023-10-14 17:00:21 +0200

    Fizz 3 e Buzz 5

--- js/script.js
+++ js/script.js
@@ -11,16 +11,28 @@ for (let i = 1; i <= 100; i++) {
   const box = document.createElement('div');
   box.classList.add('box');
   
-//collegamento tra boxes e box
+//collegamento tra boxes e box usando append()
   boxes.append(box);
   
-  //i numeri  sia multipli di 3 che di 5  “FizzBuzz” usando la classe append()
+  //i numeri sia multipli di 3 che di 5  “FizzBuzz” usando la classe append()
   if (i % 15 === 0) {
     
     box.classList.add('ColFizzbuzz');
     box.append('FizzBuzz');
 
+    //numeri multimpli di 3 Fizz
+  } else if (i % 3 === 0) {
+    
+    box.classList.add('ColFizz');
+    box.append('Fizz');
+    
+    //Numweri multipli di 5 Buzz
+  } else if (i % 5 === 0 ) {
    
+    box.classList.add('ColBuzz');
+    box.append('Buzz');
+
+
    
   }
 
